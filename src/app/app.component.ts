9d0ea9b3eb1eed092b1ca589911b5f5d9fd81fbf
Dany Paredes <danywalls4@gmail.com>
2024-12-04 20:18:51 +0100

    install kendo_layout and render commits - set initial data in the signals. - remove actions props in the response.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,23 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {KENDO_TIMELINE} from "@progress/kendo-angular-layout";
+import {toSignal} from "@angular/core/rxjs-interop";
+import {GithubService} from "./services/github.service";
+import {DatePipe} from "@angular/common";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, KENDO_TIMELINE, DatePipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'github-timeline';
+  githubService = inject(GithubService);
+  public commits$ = toSignal(this.githubService.getCommits('alyssamichelle', 'tour-of-ponies'),
+    {
+      initialValue: []
+    });
+
 }
---
+++ src/app/services/github.service.ts
@@ -0,0 +1,25 @@
+import {inject, Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {map, Observable} from 'rxjs';
+import {TimelineEvent} from "@progress/kendo-angular-layout";
+
+@Injectable({
+  providedIn: 'root',
+})
+export class GithubService {
+  private githubRepo = 'https://api.github.com/repos';
+  private http = inject(HttpClient);
+  getCommits(user: string, repo: string): Observable<TimelineEvent[]> {
+    return this.http.get(`${this.githubRepo}/${user}/${repo}/commits`).pipe(
+      map((response: any) => {
+        return response.map((commit: any) => ({
+            title: commit.commit.message,
+            subtitle: commit.commit.author.date,
+            description: commit.commit.author.name,
+            date: new Date(commit.commit.author.date)
+          }
+        ));
+      })
+    );
+  }
+}
