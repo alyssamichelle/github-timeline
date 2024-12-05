a5701cdf3582ae148b061d1281eec087b88fb83c
Alyssa Nicoll <mail@alyssanicoll.com>
2024-12-05 14:31:43 -0600

    updating to follow along with post rewrite

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,18 @@
 import {Component, inject} from '@angular/core';
-import {RouterOutlet} from '@angular/router';
 import {KENDO_TIMELINE} from '@progress/kendo-angular-layout';
 import {GithubService} from './services/github.service';
-import {DatePipe} from "@angular/common";
 import {toSignal} from "@angular/core/rxjs-interop";
 
 @Component({
     selector: 'app-root',
-    imports: [RouterOutlet, KENDO_TIMELINE, DatePipe],
+    standalone: true,
+    imports: [KENDO_TIMELINE],
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss'
 })
 export class AppComponent {
     githubService = inject(GithubService);
-    public commits$ = toSignal(this.githubService.getCommits('alyssamichelle', 'tour-of-ponies'));
+    public commits$ = toSignal(this.githubService.getCommits('alyssamichelle', 'tour-of-ponies'),{
+      initialValue: []
+    });
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,10 @@
 import { ApplicationConfig } from '@angular/core';
+import { provideNoopAnimations } from '@angular/platform-browser/animations';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideHttpClient()],
+  providers: [provideRouter(routes), provideHttpClient(), provideNoopAnimations()],
 };
